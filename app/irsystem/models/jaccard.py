df8feaaaa87484ae02f11311e996c8cf49ebfaea
Michael Herbstman <michaelherbstman@dhcp-rhodes-2967.eduroam.cornell.edu>
2018-04-16 17:48:51 -0400

    minor changes to jaccard.py

--- app/irsystem/models/jaccard.py
+++ app/irsystem/models/jaccard.py
@@ -1,34 +1,27 @@
 from parsers_and_TFidf_setup import *
 
 def top_jaccard_sim(post_dic, input_vec, td_mat):
-    # print (input_vec)
     top_posts = []
     jaccard_sims = []
     scores = []
-    #print (type (td_mat))
-    #print (td_mat)
+
     for row in td_mat:
         try:
-            jaccard_sims.append(len(np.intersect1d(input_vec, row)) / len(np.union1d(input_vec, row)))
+            jaccard_sims.append(float(len(np.intersect1d(input_vec, row))) / float(len(np.union1d(input_vec, row))))
         except:
             jaccard_sims.append(0)
-    
-    print (jaccard_sims)
-    
+
     sorted_indicies = np.argsort(jaccard_sims)[::-1]
-    #instead of returning the raw scores, talk to jake about using an index
-    #to convert the scores back into the original posts. This may involve
-    #creating a separate dictionary in the function that maps post_id -> sim_score.
+
     for i in range(0, 5):
         if(sorted_indicies[i] in post_dic):
             top_posts.append(post_dic[sorted_indicies[i]])
             scores.append(jaccard_sims[sorted_indicies[i]])
-            
+
     return top_posts,scores
 
 def top_n_tags(n, top_posts):
     tags = []
-    #print (top_posts)
     sorted_posts = sorted(top_posts, key = lambda x : x['numberLikes'], reverse=True)
     for post in sorted_posts:
         for tag in post['tags']:
@@ -51,11 +44,8 @@ def cleanup(keywords):
 def input_vec(word_to_int_dict, location, keywords):
     vec = np.zeros(len(word_to_int_dict))
     locs = re.findall('[a-z]+', location.lower())
-    # print ("input before cleanup:", keywords)
     words = cleanup(keywords)
-    # print ("input after cleanup:", words)
     for w in words:
-        # print ("w in words:", w)
         if w in word_to_int_dict:
             vec[word_to_int_dict[w]] = 1
     return vec
@@ -69,28 +59,19 @@ def input_to_tags(location, keywords):
     in_vec = input_vec(word_to_int_dict, location, keywords)
 
     top_jaccard_posts,scores = top_jaccard_sim(post_dict, in_vec, word_TDF)
-    #print (scores)
-    #print (top_jaccard_posts[0])
-    
     tags = top_n_tags(num_tags, top_jaccard_posts)
-    
     fallback_tag_lst = fallback_tags(keywords, loc="")
-    
+
     for i in range (num_tags):
         if scores[i] < 1:
              tags[i] = fallback_tag_lst[i]
-            
-    
+
+
     return tags
 
 if __name__ == "__main__":
-# #He may be old and he may not be a ginger but he is my brother #nofilter #goodvibesonly
     tags = input_to_tags("", "he may be old and he may not be a ginger")
     print(tags)
-#  
- 
- 
- 
     # word_to_int_dict, tag_to_int_dict, int_to_word_dict, int_to_tag_dict, word_TDF,\
     # tag_TDF, word_inv_idx, tag_inv_idx, post_dict = process_list_of_jsons(['profile_davidmiron.json'])
     # print (post_dict)
