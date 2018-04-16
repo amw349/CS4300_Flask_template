ccc491428728453d989ee17c288e55c1e05092bd
Michael Herbstman <michaelherbstman@dhcp-clara-6505.eduroam.cornell.edu>
2018-04-16 01:25:50 -0400

    minor changes

--- jaccard.py
+++ jaccard.py
@@ -1,25 +1,31 @@
-from Parsers_TFidf_setup import *
+from parsers_and_TFidf_setup import *
 
 def top_jaccard_sim(post_dic, input_vec, td_mat):
+    print (input_vec)
     top_posts = []
     jaccard_sims = []
     for row in td_mat:
-        jaccard_sims.append(len(np.intersect1d(input_vec, td_mat[i])) / len(np.union1d(input_vec, td_mat[i])))
+        try:
+            jaccard_sims.append(len(np.intersect1d(input_vec, row)) / len(np.union1d(input_vec, row)))
+        except:
+            jaccard_sims.append(0)
 
     jaccard_sims = np.argsort(jaccard_sims)
     #instead of returning the raw scores, talk to jake about using an index
     #to convert the scores back into the original posts. This may involve
     #creating a separate dictionary in the function that maps post_id -> sim_score.
     for i in range(0, 10):
-        top_posts.append(post_dic[jaccard_sims[i]])
+        if(jaccard_sims[i] in post_dic):
+            top_posts.append(post_dic[jaccard_sims[i]])
 
     return top_posts
 
 def top_n_tags(n, top_posts):
     tags = []
-    sorted_posts = sorted(top_posts, key = lambda x : x['numberLikes'])
+    #print (top_posts)
+    sorted_posts = sorted(top_posts, key = lambda x : x['numberLikes'], reverse=True)
     for post in sorted_posts:
-        for tag in post[i]['tags']:
+        for tag in post['tags']:
             tags.append(tag)
 
     return tags[:n]
@@ -30,7 +36,7 @@ def cleanup(keywords):
     for word in keywords:
         for char in word:
             if char.isalpha():
-                lst.append(i.lower())
+                lst.append(char.lower())
         s = "".join(lst)
         lst = []
         processed.append(s)
@@ -50,9 +56,12 @@ def input_vec(word_to_int_dict, location, keywords):
 def input_to_tags(location, keywords):
     word_to_int_dict, tag_to_int_dict, int_to_word_dict, int_to_tag_dict, word_TDF,\
     tag_TDF, word_inv_idx, tag_inv_idx, post_dict = process_list_of_jsons(['profile_davidmiron.json'])
+
     in_vec = input_vec(word_to_int_dict, location, keywords)
-    return top_n_tags(10, top_jaccard_sim(post_dict, in_vec, word_TDF))
+    return top_n_tags(3, top_jaccard_sim(post_dict, in_vec, word_TDF))
 
 if __name__ == "__main__":
-    word_to_int_dict, tag_to_int_dict, int_to_word_dict, int_to_tag_dict, word_TDF,\
-    tag_TDF, word_inv_idx, tag_inv_idx, post_dict = process_list_of_jsons(['profile_davidmiron.json'])
+    tags = input_to_tags("barcelona", ["ami", "selfies", "dads", "ginger"])
+    print(tags)
+    #word_to_int_dict, tag_to_int_dict, int_to_word_dict, int_to_tag_dict, word_TDF,\
+    #tag_TDF, word_inv_idx, tag_inv_idx, post_dict = process_list_of_jsons(['profile_davidmiron.json'])
