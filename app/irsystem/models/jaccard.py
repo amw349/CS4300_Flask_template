1e00c627189bb35d5a0155cd44e72bd0cf2c18a6
Michael Herbstman <michaelherbstman@michaels-mbp-2.home>
2018-04-16 21:51:07 -0400

    jaccard change to load many jsons in a directory

--- app/irsystem/models/jaccard.py
+++ app/irsystem/models/jaccard.py
@@ -54,6 +54,12 @@ def input_vec(word_to_int_dict, location, keywords):
 
 def input_to_tags(location, keywords):
     num_tags = 3
+    #profile_lst = []
+    # for subdir, dirs, files in os.walk('./'):
+    #     for file in files:
+    #         if file[(len(file)-5):(len(file))]=='.json':
+    #             profile_lst.append(file)
+    #print(profile_lst)
     profile_lst = ['profile_davidmiron.json']
     word_to_int_dict, tag_to_int_dict, int_to_word_dict, int_to_tag_dict, word_TDF,\
     tag_TDF, word_inv_idx, tag_inv_idx, post_dict = process_list_of_jsons(profile_lst)
