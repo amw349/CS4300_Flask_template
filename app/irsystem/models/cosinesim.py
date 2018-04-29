294b84f4efb63541d02e9c0c6750bc689cf081d4
Michael Herbstman <michaelherbstman@dhcp-clara-7931.eduroam.cornell.edu>
2018-04-29 12:09:24 -0400

    minor changes

--- app/irsystem/models/cosinesim.py
+++ app/irsystem/models/cosinesim.py
@@ -17,21 +17,14 @@ def cleanup(input_text):
     return processed
 
 def top_n_tags(top_posts, int_to_word_dict, n=10):
-    # tags = []
-    # for post in top_posts:
-    #     for tag in post['tags']:
-    #         tags.append(tag)
-    # return tags[:n]
     words = []
     for word in top_posts:
         words.append(int_to_word_dict)
 
     return words[:10]
 
-def input_vec(word_to_int_dict, keywords):
-    vec = np.zeros(len(word_to_int_dict))
-    #locs = re.findall('[a-z]+', location.lower())
-    #words = cleanup(keywords)
+def input_vec(td_mat, word_to_int_dict, keywords):
+    vec = np.zeros(td_mat.shape[0])
     for w in keywords:
         if w in word_to_int_dict:
             vec[word_to_int_dict[w]] = 1
@@ -61,32 +54,19 @@ def input_to_tags(input_text, td_mat, word_to_int_dict, post_dict, int_to_word_d
 
     cosine_sims = np.argsort(cosine_sims)
     top_words = [int_to_word_dict[i] for i in cosine_sims]
-    vec = input_vec(word_to_int_dict, top_words[:10])
+    vec = input_vec(td_mat, word_to_int_dict, top_words[:10])
 
-    #for i in range(0, td_mat.shape[1]):
+    post_scores = []
+    for i in range(0, td_mat.shape[1]):
+        sim = np.dot(td_mat[:,i], vec)
+        sim = sim / ((LA.norm(vec)) * (LA.norm(td_mat[:,i])))
+        post_scores.append(sim)
+    post_scores = np.argsort(post_scores)
+    print(len(post_dict))
+    top_tags = [post_dict[j]['tags'] for j in post_scores]
     return []
 
-    # top_posts = {}
-    # for i in range(0, len(post_vecs)):
-    #     for j in range(0, td_mat.shape[1]):
-    #         col = td_mat[:,j]
-    #         cossim = np.dot(col, post_vecs[i])
-    #         cossim = cossim / ((LA.norm(col)) * (LA.norm(post_vecs[i])))
-    #         if(top_words[i] in top_posts):
-    #             top_posts[top_words[i]].append(cossim)
-    #         else:
-    #             top_posts[top_words[i]] = []
-    #             top_posts[top_words[i]].append(cossim)
-    #     top_posts[top_words[i]] = np.argsort(top_posts[top_words[i]])
-    #     top_posts[top_words[i]] = [post_dict[i]['tags'] for i in top_posts[top_words[i]]]
-
 if __name__ == "__main__":
-    # profile_lst = []
-    # for subdir, dirs, files in os.walk('/Users/michaelherbstman/Desktop/CS_4300/Project/CS4300_Flask_template/app/static/json/'):
-    #     for file in files:
-    #         if (file.endswith('.json')) and (os.getcwd().endswith('old') == False):
-    #             profile_lst.append(file)
-    #print(profile_lst)
     word_to_int_dict, tag_to_int_dict, int_to_word_dict, int_to_tag_dict, word_TDF,\
     tag_TDF, word_inv_idx, tag_inv_idx, post_dict = process_list_of_jsons(['atighteru.json', 'balous_friends.json'])
 
