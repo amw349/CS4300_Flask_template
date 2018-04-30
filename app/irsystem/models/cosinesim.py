bd898c2f8ad97178118886f109c0b3015c5dbda5
Michael Herbstman <michaelherbstman@dhcp-rhodes-3242.eduroam.cornell.edu>
2018-04-30 19:18:28 -0400

    fix an svd issue

--- app/irsystem/models/cosinesim.py
+++ app/irsystem/models/cosinesim.py
@@ -74,15 +74,12 @@ def input_vec(td_mat, word_to_int_dict, keywords):
     return vec
 
 def input_to_tags(input_text, td_mat, word_to_int_dict, post_dict, int_to_word_dict, k=10):
-    print("MIGHT BE WORKING")
     cosine_sims=[]
     top_posts=[]
     count=0
     keywords = cleanup(input_text)
-    print("td mat is", td_mat)
-    # docs_compressed, x, words_compressed = svds(td_mat.astype(float), k=10)
-    print("MIGHT BE WORKING")
     words_compressed = np.load('words_compressed.npy')
+    words_compressed = np.transpose(words_compressed)
     words_compressed = normalize(words_compressed, axis = 1)
     avg_input_vec = np.zeros(words_compressed.shape[1])
 
@@ -90,7 +87,6 @@ def input_to_tags(input_text, td_mat, word_to_int_dict, post_dict, int_to_word_d
         if word in word_to_int_dict:
             count = count + 1
             avg_input_vec = avg_input_vec + words_compressed[word_to_int_dict[word]]
-
     try:
         avg_input_vec = avg_input_vec / count
     except:
